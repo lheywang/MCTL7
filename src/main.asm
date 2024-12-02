2754240c9dd1aec52332d2793a986c72b652fbff
Léonard Heywang <l.heywang@ad.unistra.fr>
2024-12-02 11:49:01 +0100

    Output buffer is working !

--- src/main.asm
+++ src/main.asm
@@ -15,13 +15,28 @@
     ORG	    0x000
     NOP
     
+; ------------------------------------------------------------------------------
+; JUMP TO INIT
+; ------------------------------------------------------------------------------
+    
     GOTO	init
     
 ; ------------------------------------------------------------------------------
 ; MEMORY
 ; ------------------------------------------------------------------------------
+    ; General usage variables
     CVT		EQU	    20h
     CNT		EQU	    21h
+    SUM		EQU	    22h
+    SAV		EQU	    23h
+    CNT2	EQU	    24h
+
+    ; Output buffer
+    OUT1	EQU	    30h
+    OUT2	EQU	    31h
+    OUT3	EQU	    32h
+    OUT4	EQU	    33h	
+    OUT5	EQU	    34h
     
 ; ------------------------------------------------------------------------------
 ; ADC
@@ -122,6 +137,30 @@ USART_SEND:
     
     RETURN			    ; End of function
     
+USART_SEND_ADC_BUF:		    ; Send the ADC output buffer, char by char and add \r\n
+    MOVFW	    OUT1
+    CALL	    USART_SEND
+    
+    MOVFW	    OUT2
+    CALL	    USART_SEND
+    
+    MOVFW	    OUT3
+    CALL	    USART_SEND
+    
+    MOVFW	    OUT4
+    CALL	    USART_SEND
+    
+    MOVFW	    OUT5
+    CALL	    USART_SEND
+    
+    MOVLW	    '\r'
+    CALL	    USART_SEND
+    
+    MOVLW	    '\n'
+    CALL	    USART_SEND
+    
+    RETURN
+    
 ; ------------------------------------------------------------------------------
 ; CONVERSION
 ; ------------------------------------------------------------------------------
@@ -129,28 +168,79 @@ GET_VOLT:
     ; Get the integer part of the voltage
     ; Used register : W
     ; Input register : W
-    ; Return register : W
+    ; Return register : OUT1
     
     BANKSEL	    TMR0	    ; Dummy selection of the bank 0$
     
-    MOVWF	    CVT		    ; Place W into CVT
+    MOVWF	    CVT		    ; Place W into CVT and SAV
+    MOVWF	    SAV
     
     MOVLW	    .0
-    MOVWF	    CNT		    ; Initialize counter at 0
+    MOVWF	    CNT		    ; Initialize counters at 0
+    MOVWF	    CNT2
+    
+    MOVWF	    OUT1	    ; Clearing the output buffer
+    MOVWF	    OUT2
+    MOVWF	    OUT3
+    MOVWF	    OUT4
+    MOVWF	    OUT5
+	
+    MOVWF	    SUM		    ; Clearing the sum
 
-CVT_SUB:
+CVT_SUB:			    ; Routine to get the integer part of the measure
     CLRC			    ; Clear carry
     MOVLW	    .51		    ; Place 51 in the W register
     SUBWF	    CVT,    W	    ; Remove 51 of CVT
     MOVWF	    CVT
     
-    BNC		    CVT_END	    ; Exit if negative found
+    BNC		    CVT_INT	    ; Exit if negative found
     
     INCF	    CNT		    ; If not carry nor zero : Increment and restart
-    GOTO	    CVT_SUB
+    
+    MOVFW	    SUM		    ; Increment the integer part register
+    ADDLW	    .51
+    MOVWF	    SUM
+    
+    GOTO	    CVT_SUB	    ; Jump
+	
+CVT_INT:
+    MOVFW	    CNT
+    ADDLW	    30h		    ; Convert to ASCII character
+    
+    MOVWF	    OUT1	    ; Copy into output buffer
+    
+    MOVFW	    SUM		    ; Substract the sum of 551 to the input value
+    SUBWF	    SAV		    ; Now, SAV contain the difference between integer and input.
+
+CVT_SUB2:
+    MOVLW	    .5		    ; Place 5 in the W register
+    SUBWF	    SAV,    W	    ; Remove 51 of CVT
+    MOVWF	    SAV
+    
+    BNC		    CVT_END	    ; Exit if negative found
+    
+    INCF	    CNT2	    ; If not carry nor zero : Increment and restart
+    
+    GOTO	    CVT_SUB2	    ; Jump
     
 CVT_END:
-    MOVFW	    CNT		    
+    MOVFW	    CNT
+    ADDLW	    30h
+    MOVWF	    OUT1
+    
+    MOVLW	    ','
+    MOVWF	    OUT2
+    
+    MOVFW	    CNT2
+    ADDLW	    30h
+    MOVWF	    OUT3
+    
+    MOVLW	    ' '
+    MOVWF	    OUT4
+    
+    MOVLW	    'V'
+    MOVWF	    OUT5
+    
     RETURN
     
 ; ------------------------------------------------------------------------------
@@ -171,10 +261,10 @@ init:
 start:
     CALL	    ADC_GET
     MOVFW	    ADRESH	    ; Copy the result into W
-    CALL	    GET_VOLT
-    ADDLW	    30h
     
-    CALL	    USART_SEND
+    CALL	    GET_VOLT
+
+    CALL	    USART_SEND_ADC_BUF	; Write the output buffer
     GOTO	    start
     
     END				     ; Fin du code
